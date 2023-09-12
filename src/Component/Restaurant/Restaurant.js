33029466eac7c1ae61cd3133afbf246a7df8c59c
Mohammad Rubel Mollik <rubel@pop-os.localdomain>
2023-09-13 00:07:53 +0600

    first commit

---
+++ src/Component/Restaurant/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Restaurant = () => {
+    return (
+        <div>
+            <h1 style={{color:'red',fontSize:'50px'}}>Find the food you want</h1>
+
+        </div>
+    );
+};
+
+export default Restaurant;
