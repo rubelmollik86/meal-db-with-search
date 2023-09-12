c6b1951f87233a61fb9a1cf0c5c5108225048eec
Mohammad Rubel Mollik <rubel@pop-os.localdomain>
2023-09-13 00:55:56 +0600

    set search meal db api

--- src/Component/Restaurant/Restaurant.js
+++ src/Component/Restaurant/Restaurant.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Restaurant = () => {
+    const [searchText, setSearchText] = useState(''); 
+
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMeals(data.meals));
+    }, [searchText]);
+
+    const searchFood = e => {
+       setSearchText(e.target.value);
+    }
     return (
         <div>
-            <h1 style={{color:'red',fontSize:'50px'}}>Find the food you want</h1>
-
+            <h1 style={{ color: 'red', fontSize: '50px' }}>Find the food you want</h1>
+            <input onChange={searchFood} type='text' name='' id='' placeholder='search'></input>
+            <br />
+            
+            <h3>Result found: { meals.length}</h3>
         </div>
     );
 };
